015f92fdfa4103a2a9b5441c4b95b737720397f7
dreslerek <39015811+dreslerek@users.noreply.github.com>
2019-06-02 14:43:02 +0200

    Add files via upload

---
+++ TTT.Tests/Class1.cs
@@ -0,0 +1,109 @@
+﻿using NUnit.Framework;
+
+namespace TTT.Tests
+{
+    [TestFixture]
+    public class UserOrderServiceTests
+    {
+        [TestCase]
+        public void When_PremiumUser_Expect_10PercentDiscount()
+        {
+            User premiumUser = new User
+            {
+                UserId = 1,
+                UserName = "George",
+                UserType = UserType.Premium
+            };
+
+            Order order = new Order
+            {
+                OrderId = 1,
+                ProductId = 212,
+                ProductQuantity = 1,
+                Amount = 150
+            };
+
+            UserOrderService userOrderService = new UserOrderService();
+
+            userOrderService.ApplyDiscount(premiumUser, order);
+
+            Assert.AreEqual(order.Amount, 135);
+        }
+        [TestCase]
+        public void When_NameContainsPolishChars_Expect_ContainsPolishChars_ReturnsTrue()
+        {
+            User user = new User
+            {
+                UserId = 2,
+                UserName = "Michał",
+                UserType = UserType.Premium
+            };
+
+            var result = user.ContainsPolishChars();
+
+            Assert.IsTrue(result);
+        }
+        [TestCase]
+        public void When_NameDoesNotContainPolishChars_Expect_DoesNotContainPolishChars_ReturnsFalse()
+        {
+            User user = new User
+            {
+                UserId = 2,
+                UserName = "Wojtek",
+                UserType = UserType.Premium
+            };
+
+            var result = user.ContainsPolishChars();
+
+            Assert.IsFalse(result);
+        }
+
+    }
+    [TestFixture]
+    public class UserInfoTests
+    {
+        [TestCase]
+        public void When_PasswordIsLongEnough_Expect_PasswordIsLongEnough_ReturnsTrue()
+        {
+            User user = new User
+            {
+                password = "12345678"
+            };
+
+            var result = user.IsPasswordLongEnough();
+
+            Assert.IsTrue(result);
+        }
+        [TestCase]
+        public void When_PasswordIsLongEnough_Expect_PasswordIsNotLongEnough_ReturnsFalse()
+        {
+            User user = new User
+            {
+                password = "1234"
+            };
+
+            var result = user.IsPasswordLongEnough();
+
+            Assert.IsFalse(result);
+        }
+        [TestCase(29, ExpectedResult = false)]
+        [TestCase(0, ExpectedResult = false)]
+        [TestCase(60, ExpectedResult = true)]
+        [TestCase(80, ExpectedResult = true)]
+        [TestCase(90, ExpectedResult = true)]
+        public bool When_AgeGreaterAndEqualTo60_Expects_IsSeniorCitizenAsTrue(int age)
+        {
+            User user = new User();
+            user.Age = age;
+
+            bool result = user.IsSeniorCitizen();
+
+            return result;
+        }
+
+
+    }
+
+
+
+    }
---
+++ TTT/Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TTT
+
+{
+    public enum UserType
+    {
+        Basic,
+        Premium,
+        SpecialUser
+    }
+
+
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public UserType UserType { get; set; }
+        public string password { get; set; }
+        public int Age { get; set; }
+
+
+        public bool IsSeniorCitizen()
+        {
+            if (Age >= 60)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool ContainsPolishChars()
+        {
+            if (this.UserName.Contains("ą") | this.UserName.Contains("ć") | this.UserName.Contains("ś") | this.UserName.Contains("ł")
+                | this.UserName.Contains("ó") | this.UserName.Contains("ę") | this.UserName.Contains("ż") | this.UserName.Contains("ź")
+                | this.UserName.Contains("ń"))
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool IsPasswordLongEnough()
+        {
+            if (this.password.Length > 7)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int ProductQuantity { get; set; }
+        public decimal Amount { get; set; }
+    }
+
+    public class UserOrderService
+    {
+        public void ApplyDiscount(User user, Order order)
+        {
+            if (user.UserType == UserType.Premium)
+            {
+                order.Amount = order.Amount - ((order.Amount * 10) / 100);
+            }
+            else if (user.UserType == UserType.SpecialUser)
+            {
+                order.Amount = order.Amount - ((order.Amount * 20) / 100);
+            }
+            else if (user.UserType == UserType.Basic)
+            {
+                order.Amount = order.Amount - ((order.Amount * 30) / 100);
+            }
+        }
+    }
+}
